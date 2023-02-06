71d56366c5c6a7ec3ca8f1ee2bab5d9152931153
Philip Giles <philip.giles@portable.com.au>
2023-02-06 16:34:04 +1100

    Changed to switch statements. Added typing indicator support.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { bot } from './telegramBot.js';
 import { api } from './gpt.js';
 import express from 'express';
 import http from 'http';
+import _ from 'lodash';
 
-dotenv.config({ path: '../config/settings.env' });
+dotenv.config({ path: './config/settings.env' });
 
 let conversationId;
 let lastMsgId;
@@ -20,9 +21,10 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-
 const server = http.createServer(app);
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${process.env.PORT || 8080}`);
+});
 
 let firstMessage = true;
 
@@ -32,46 +34,58 @@ bot.on('message', async (msg) => {
 
   if (firstMessage) {
     firstMessage = false;
-  bot.sendMessage(chatId, helpMessage, { parse_mode: "Markdown" });
-  }
-
-  if (messageText === '/reset') {
-    conversationId = undefined;
-    bot.sendMessage(chatId, 'Chat reset.');
-    return;
-  }
-
-  if (messageText === '/retry') {
-    if (!conversationId) {
-      bot.sendMessage(chatId, 'No message to retry.');
-      return;
-    }
-    const response = await api.sendMessage(messageText, {
-      conversationId: conversationId,
-      parentMessageId: lastMsgId
-    });
-    bot.sendMessage(chatId, response.text);
-    return;
-  }
-
-  if (messageText === '/help') {
     bot.sendMessage(chatId, helpMessage, { parse_mode: "Markdown" });
-    return;
   }
 
-  let response;
+  switch (messageText) {
+    case '/reset': {
+      conversationId = undefined;
+      lastMsgId = undefined;
+      bot.sendMessage(chatId, 'Chat reset.');
+      break;
+    }
+    case '/retry': {
+      if (!conversationId) {
+        bot.sendMessage(chatId, 'No message to retry.');
+        break;
+      }
+      const response = await api.sendMessage(messageText, {
+        conversationId: conversationId,
+        parentMessageId: lastMsgId
+      });
+      bot.sendMessage(chatId, response.text, {parse_mode: "Markdown"});
+      break;
+    }
+    case '/help': {
+      bot.sendMessage(chatId, helpMessage, { parse_mode: "Markdown" });
+      break;
+    }
+    default: {
+      let response;
+      let sentMsgId;
 
-  if (conversationId) {
-    response = await api.sendMessage(messageText, {
-      conversationId: conversationId,
-      parentMessageId: lastMsgId
-    });
-    lastMsgId = response.id;
-  } else {
-    response = await api.sendMessage(messageText);
-    conversationId = response.conversationId;
-    lastMsgId = response.id;
+      bot.sendMessage(chatId, ". . .").then((sentMsg) => {
+        sentMsgId = sentMsg.message_id;
+      });
+      
+      if (conversationId) {
+        response = await api.sendMessage(messageText, {
+          onProgress: _.throttle(() => bot.sendChatAction(chatId, "typing"), 4000),
+          conversationId: conversationId,
+          parentMessageId: lastMsgId
+        });
+        lastMsgId = response.id;
+
+      } else {
+        
+        response = await api.sendMessage(messageText, { 
+          onProgress: _.throttle(() => bot.sendChatAction(chatId, "typing"), 4000),
+        });
+        conversationId = response.conversationId;
+        lastMsgId = response.id;
+      }
+      bot.editMessageText(response.text, { chat_id: chatId, message_id: sentMsgId, parse_mode: "Markdown" });
+      break;
+    }
   }
-
-  bot.sendMessage(chatId, response.text);
 });
--- src/telegramBot.js
+++ src/telegramBot.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
 
-dotenv.config({ path: '../config/settings.env' });
+dotenv.config({ path: './config/settings.env' });
 
 const telegram_token = process.env.TELEGRAM_API_KEY;
 
